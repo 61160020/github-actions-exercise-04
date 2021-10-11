b46fdcdc17239a28c08b790a0ab6945ffcac1020
Warakorn Bunhlua <61160020@go.buu.ac.th>
2021-10-11 16:09:43 +0700

    Setup app v.3

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('GET /', () => {
-    it('{"message":"Warakorn 61160020"}', (done) => {
-        request(app).get('/').expect('{"Warakorn 61160020"}', done)
+    it('"Warakorn 61160020', (done) => {
+        request(app).get('/').expect('Warakorn 61160020', done)
     })
 })
